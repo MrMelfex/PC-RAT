489fac58749ec64b51781d44beba3155dc71c956
Mr-Melfex <149225543+MrMelfex@users.noreply.github.com>
2023-12-01 08:37:53 +0330

    Modulea

---
+++ Modules/Modules.py
@@ -0,0 +1,12 @@
+# [~~] Mudules.py
+# Codeing "UTF-8"
+# CopyRight (C) 2023 By *Mr-Melfex*
+# License BCD (SEE LICENSE IN MAIN FOLDER)
+
+try:
+    import proxy, cv2, json, mss, pytz, wave, pyaudio, pyttsx3, win32crypt, shutil, urllib.request, sqlite3, os, time, zipfile, sys, telebot, pywifi, platform, requests, string, re, socket, ctypes, subprocess, random
+    from json import loads
+    from pytz import timezone
+
+except ImportError:
+    raise SystemExit ("[O_O] ~ Pls Import Mudules ! >> pip install r-req.txt")
