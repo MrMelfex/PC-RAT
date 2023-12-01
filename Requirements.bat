9e4aafda5e9a460f53c42a50d9bd9e806757c2cf
Mr-Melfex <149225543+MrMelfex@users.noreply.github.com>
2023-12-01 08:43:49 +0330

    AU Requirements

---
+++ Requirements.bat
@@ -0,0 +1,17 @@
+echo off
+
+pip3 install pytelegrambotapi
+pip3 install pycryptodome
+pip3 install pyperclip
+pip3 install keyboard
+pip3 install pyttsx3
+pip3 install pywin32
+pip3 install pynput
+pip3 install mss
+pip3 install pyinstaller
+pip3 install telebot
+pip3 install proxy
+pip3 install soket
+
+:cmd
+pause null
